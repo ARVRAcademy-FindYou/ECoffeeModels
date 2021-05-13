4193f3a14527dd41052fe53150f2878d37f80e08
Yilin Wang <yiwa0808@uw.edu>
2021-05-13 00:18:12 -0700

    modify import

--- src/main/java/com/ecoffee/ecoffee_service/services/orderService/models/Order.java
+++ src/main/java/com/ecoffee/ecoffee_service/services/orderService/models/Order.java
@@ -1,6 +1,6 @@
 package com.ecoffee.ecoffee_service.services.orderService.models;
 
-import com.ecoffee.ecoffee_service.services.paymentService.model.OrderTax;
+import com.ecoffee.ecoffee_service.services.paymentService.models.OrderTax;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
