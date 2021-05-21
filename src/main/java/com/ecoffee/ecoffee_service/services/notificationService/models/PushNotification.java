e5b598a50925173a970a5059c27b17fc6e8ae3e8
Shang Li <shangji@amazon.com>
2021-05-20 23:30:52 -0700

    Add builder default

--- src/main/java/com/ecoffee/ecoffee_service/services/notificationService/models/PushNotification.java
+++ src/main/java/com/ecoffee/ecoffee_service/services/notificationService/models/PushNotification.java
@@ -6,15 +6,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.mongodb.morphia.annotations.Id;
 
+import java.util.UUID;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
 public class PushNotification {
-    @Id
-    private String id;
+    @Builder.Default
+    private String id = UUID.randomUUID().toString();
 
-    private String title;
-    private String content;
+    @Builder.Default
+    private String title = "No title";
+
+    @Builder.Default
+    private String content = "<Empty content>";
 }
